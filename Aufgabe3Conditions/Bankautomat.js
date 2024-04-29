ebb2f9abf33382b8ac9ad5a39201f56d592ec6c9
Janify <platzjana@gmail.com>
2024-04-29 11:07:48 +0200

    easy print system that prints out a certain text thats predetermined

--- Aufgabe3Conditions/Bankautomat.js
+++ Aufgabe3Conditions/Bankautomat.js
@@ -3,3 +3,18 @@ const No2 = 200 ;
 const No3 = 500 ;
 let No4 = 10;
 
+if ( No1 === true ){
+    console.log("100€ werden ausbezahlt!")
+}
+
+else if ( No2 === true ){
+    console.log("200€ werden ausbezahlt!")
+}
+
+else if ( No3 === true ){
+    console.log("500€ werden ausbezahlt!")
+}
+
+else {
+    console.log(No4 + "€ werden ausbezahlt!")
+};
