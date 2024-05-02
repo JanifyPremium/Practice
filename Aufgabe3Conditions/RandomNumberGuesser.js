53e246390f234e27280ddb04aedda5c4a2d5cb57
Janify <platzjana@gmail.com>
2024-05-02 14:01:17 +0200

    createt a random number guessing game loop is still to be fixed since it will run infinite

--- Aufgabe3Conditions/RandomNumberGuesser.js
+++ Aufgabe3Conditions/RandomNumberGuesser.js
@@ -1,2 +1,18 @@
 let attempt = 0;
-let Number = 0;
+let Number = ((Math.random()*100)+1);
+let Eingabe = 0;
+
+while (attempt <= 8){
+    if (Eingabe == Number){
+        console.log("Du hast gewonnen");
+    }
+    else if (Eingabe< Number){
+        console.log("Deine Zahl ist zu niedrig");
+    }
+    else if (Eingabe> Number){
+        console.log("Deine Zahl ist zu hoch");
+    }
+    else{
+        console.log("Falsche Eingabe");
+    };
+};
