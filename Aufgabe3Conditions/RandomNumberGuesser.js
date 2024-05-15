e2dd31a3728c8073fb4b06738d6d392699743454
Janify <107705505+JanifyPremium@users.noreply.github.com>
2024-05-15 15:36:38 +0200

    Update RandomNumberGuesser.js
    added an upwards counting to attempt variable to stop it from running infinite still needs some fixes

--- Aufgabe3Conditions/RandomNumberGuesser.js
+++ Aufgabe3Conditions/RandomNumberGuesser.js
@@ -1,15 +1,17 @@
 let attempt = 0;
 let Number = ((Math.random()*100)+1);
 let Eingabe = 0;
-
-while (attempt <= 8){
+for (attempt <= 8){
     if (Eingabe == Number){
+        attempt++;
         console.log("Du hast gewonnen");
     }
     else if (Eingabe< Number){
+        attempt++;
         console.log("Deine Zahl ist zu niedrig");
     }
     else if (Eingabe> Number){
+        attempt++;
         console.log("Deine Zahl ist zu hoch");
     }
     else{
